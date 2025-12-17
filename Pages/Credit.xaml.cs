51cf9d58fb57d9ab7d7ae91e68eca45993f58f5d
Pir6j9k <hasulik2060@gmail.com>
2025-12-17 16:15:50 +0300

    файл Credit.xaml.cs

---
+++ Pages/Credit.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP323_Khachatryan_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Credit.xaml
+    /// </summary>
+    public partial class Credit : Page
+    {
+        public Credit()
+        {
+            InitializeComponent();
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Data());
+            Info.MW.Add();
+
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Show();
+        }
+
+        private void CreditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(InitialPercentBox.Text) > 100)
+            {
+                MessageBox.Show("Введен некорректный процент", "Ошибка!");
+                return;
+            }
+            if (int.Parse(MonthsBox.Text)>96 || int.Parse(MonthsBox.Text) < 12)
+            {
+                MessageBox.Show("Введен некорректный срок", "Ошибка!");
+                return;
+            }
+            Price.n = int.Parse(MonthsBox.Text);
+            Price.procent = int.Parse(InitialPercentBox.Text);
+            Price.credit(ResultText);
+        }
+
+        private void InitialPercentBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char ch in e.Text)
+            {
+                if (!char.IsDigit(ch))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
+        }
+    }
+}
