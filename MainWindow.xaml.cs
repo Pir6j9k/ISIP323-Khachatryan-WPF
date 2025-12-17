a1d23580c9b3b5d838e8af1dc884d01a061d6ca3
Pir6j9k <hasulik2060@gmail.com>
2025-12-17 16:16:39 +0300

    главная страница MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,36 @@ namespace ISIP323_Khachatryan_WPF
         {
             InitializeComponent();
         }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (e.Content is Page page)
+            {
+                TitleTextBlock.Text = page.Title;
+            }
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+            Reduce();
+        }
+        public void Add()
+        {
+            Progress.Value += 1;
+        }
+        public void Reduce()
+        {
+            Progress.Value -= 1;
+        }
+        private void MainFrame_NavigationStopped(object sender, NavigationEventArgs e)
+        {
+            Add();
+        }
+
     }
 }
