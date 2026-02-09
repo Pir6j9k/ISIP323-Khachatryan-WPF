c18f5cfac07e5cc220a3b50383f5aad8af23e223
Pir6j9k <hasulik2060@gmail.com>
2026-02-09 08:01:58 +0300

    Создание списка продуктов List<Products>

---
+++ Carts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Khachatryan_WPF
+{
+    internal class Carts
+    {
+        public static List<Products> products = new List<Products>();
+    }
+}
