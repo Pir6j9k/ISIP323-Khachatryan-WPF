2da5bd2993c350a84cd674b5b2c498aea5ad51b3
Pir6j9k <hasulik2060@gmail.com>
2026-02-09 08:01:02 +0300

    Подключение базы данных к коду

---
+++ Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Khachatryan_WPF
+{
+    internal class Core
+    {
+        public static ShopDbEntities DB = new ShopDbEntities();
+    }
+}
