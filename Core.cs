2dc9a08b9d6af469c22c4ab7178ecaa37317161b
Pir6j9k <hasulik2060@gmail.com>
2026-03-26 14:01:07 +0300

    Класс подключения базы данных

---
+++ Core.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Khachatryan_WPF
+{
+    public class Core
+    {
+        private static PCEntities _db;
+
+        public static PCEntities DB
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new PCEntities();
+                return _db;
+            }
+        }
+    }
+}
