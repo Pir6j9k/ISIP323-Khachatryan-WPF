e90f3dcbb6b305cdc5a497de1d2ac7169ca1017f
Pir6j9k <hasulik2060@gmail.com>
2026-05-15 00:49:30 +0300

    Логика страницы информации о товаре

---
+++ Pages/Shop/ProductFullInfoPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP323_Khachatryan_WPF.Pages.Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductFullInfoPage.xaml
+    /// </summary>
+    public partial class ProductFullInfoPage : Page
+    {
+        private Product _currentProduct;
+        public ProductFullInfoPage(Product selectedProduct)
+        {
+            InitializeComponent();
+            if (selectedProduct == null)
+            {
+                MessageBox.Show("Данные о товаре не найдены");
+                if (NavigationService.CanGoBack) NavigationService.GoBack();
+                return;
+            }
+            _currentProduct = selectedProduct;
+
+            this.DataContext = selectedProduct;
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
