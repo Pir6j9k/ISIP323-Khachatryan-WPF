5a118954b04cbe6ac8df9c92a05ba9814487dd9b
Pir6j9k <hasulik2060@gmail.com>
2026-05-14 15:55:00 +0300

    Логика страницы админа

---
+++ Pages/Admin/AdminPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIP323_Khachatryan_WPF.Pages.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Page
+    {
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                DgUsers.ItemsSource = Core.DB.Users.ToList();
+            }
+        }
+
+        private void BtnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.Admin.EditUserPage(null));
+        }
+
+        private void BtnEditUser_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Tag is User selectedUser)
+            {
+                NavigationService.Navigate(new Pages.Admin.EditUserPage(selectedUser));
+            }
+        }
+
+        private void BtnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+
+            if ((sender as Button).Tag is User selectedUser)
+            {
+                if (Core.AuthUser != null && selectedUser.Id == Core.AuthUser.Id)
+                {
+                    MessageBox.Show("Вы не можете удалить свой собственный аккаунт!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                if (selectedUser.Appointments.Any() || selectedUser.Appointments1.Any())
+                {
+                    MessageBox.Show("Нельзя удалить пользователя, у которого есть записи на услуги! Сначала удалите записи.",
+                                    "Запрещено", MessageBoxButton.OK, MessageBoxImage.Stop);
+                    return;
+                }
+
+                if (MessageBox.Show($"Вы точно хотите удалить пользователя {selectedUser.Phone}?",
+                                    "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Core.DB.Users.Remove(selectedUser);
+                        Core.DB.SaveChanges();
+                        MessageBox.Show("Пользователь успешно удален!");
+
+                        DgUsers.ItemsSource = Core.DB.Users.ToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при удалении: {ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
