eb3af5ea76eacf94a2715740cc9ea98ae1f7ee3d
Pir6j9k <hasulik2060@gmail.com>
2026-05-15 00:52:21 +0300

    Частичный класс товара

---
+++ Pages/Shop/ProductPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Khachatryan_WPF
+{
+    public partial class Product
+    {
+        public bool IsHighDiscount => DiscountPercentage > 15;
+
+    }
+}
