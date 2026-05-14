2a904a43a54fbdad75f977058230add425f8fd0a
Pir6j9k <hasulik2060@gmail.com>
2026-05-14 15:54:27 +0300

    Класс импорта базы данных

---
+++ Core.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Khachatryan_WPF
+{
+    using System;
+    public class Core
+    {
+        private static TheBeautySalonEntities _db;
+
+        public static TheBeautySalonEntities DB => GetContext();
+
+        public static TheBeautySalonEntities GetContext()
+        {
+            if (_db == null)
+            {
+                _db = new TheBeautySalonEntities();
+            }
+            return _db;
+        }
+
+        public static User AuthUser = null;
+        public static List<Product> SelectedProducts = new List<Product>();
+    }
+}
