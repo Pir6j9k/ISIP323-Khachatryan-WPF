8cfd9ed67cbbdb37f8cd102a63d96425d02dca5f
Pir6j9k <hasulik2060@gmail.com>
2026-05-26 11:36:00 +0300

    Добавление справочника пользователя в проект

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -136,6 +137,10 @@ namespace ISIP323_Khachatryan_WPF
                     MainFrame.Navigate(new Pages.Salon.MainPage());
                 }
             }
-        }    
+        }
+        private void BtnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("C:Help/help.chm");
+        }
     }
 }
